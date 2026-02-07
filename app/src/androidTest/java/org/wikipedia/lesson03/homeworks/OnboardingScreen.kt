d45e92d50c2d45de83ec7016e4ac9b0cd3467214
aidarkhabibulin <AKhabibulin@alfabank.ru>
2026-02-07 15:00:44 +0300

    add homework lesson03

---
+++ app/src/androidTest/java/org/wikipedia/lesson03/homeworks/OnboardingScreen.kt
@@ -0,0 +1,58 @@
+package org.wikipedia.lesson03.homeworks
+
+import androidx.appcompat.widget.AppCompatImageView
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.tabs.TabLayout
+import org.wikipedia.R
+import org.wikipedia.views.AppTextView
+
+val skipButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button,
+    R.string.onboarding_skip
+)
+val continueButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_done_button,
+    R.string.onboarding_continue
+)
+val imageOnboardingPage = listOf(
+    AppCompatImageView::class.java,
+    R.id.imageViewCentered,
+    R.drawable.illustration_onboarding_explore
+)
+val primaryText = listOf(
+    AppTextView::class.java,
+    R.id.primaryTextView
+)
+val secondaryText = listOf(
+    AppTextView::class.java,
+    R.id.secondaryTextView
+)
+val labelText = listOf(
+    AppTextView::class.java,
+    R.id.option_label
+)
+val addLanguageButton = listOf(
+    MaterialButton::class.java,
+    R.id.addLanguageButton,
+    R.string.onboarding_multilingual_add_language_text
+)
+val tabKarusel = listOf(
+    TabLayout::class.java,
+    R.id.view_onboarding_page_indicator
+)
+
+val imageOnboardingSecondPage = listOf(
+    AppCompatImageView::class.java,
+    R.id.imageViewCentered,
+    R.drawable.illustration_onboarding_explore
+)
+val primaryTextSecondPage = listOf(
+    AppTextView::class.java,
+    R.id.primaryTextView
+)
+val secondaryTextSecondPage = listOf(
+    AppTextView::class.java,
+    R.id.secondaryTextView
+)
