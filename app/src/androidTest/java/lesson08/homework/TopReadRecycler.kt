2117b4d2f3115d834342a3f70c44be1010e4bd3d
aidarkhabibulin <AKhabibulin@alfabank.ru>
2026-03-02 19:15:52 +0300

    add homework lesson08

--- app/src/androidTest/java/lesson08/homework/TopReadRecycler.kt
+++ app/src/androidTest/java/lesson08/homework/TopReadRecycler.kt
@@ -1,10 +1,38 @@
 package lesson08.homework
 
 import android.view.View
+import io.github.kakaocup.kakao.common.views.KView
+import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
+import io.github.kakaocup.kakao.text.KTextView
 import org.hamcrest.Matcher
+import org.wikipedia.R
 
 
 class TopReadRecycler(matcher: Matcher<View>) : KRecyclerItem<TopReadRecycler>(matcher) {
+
+    val header = KTextView(matcher) {
+        withId(R.id.view_list_card_item_title)
+    }
+
+    val subtitle = KTextView(matcher) {
+        withId(R.id.view_list_card_item_subtitle)
+    }
+
+    val graph = KView(matcher) {
+        withId(R.id.view_list_card_item_graph)
+    }
+
+    val pageviews = KTextView(matcher) {
+        withId(R.id.view_list_card_item_pageviews)
+    }
+
+    val numberView = KTextView(matcher) {
+        withId(R.id.numberView)
+    }
+
+    val image = KImageView(matcher) {
+        withId(R.id.view_list_card_item_image)
+    }
 
 }
